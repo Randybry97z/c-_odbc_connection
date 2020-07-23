2c8682c1097a499f6b29171c6aaa1dd7da8e4d1a
Bryan <bssandovalrod@gmail.com>
2020-07-23 08:48:06 -0500

    Inicio de proyecto

---
+++ db/db.sql
@@ -0,0 +1,20 @@
+﻿CREATE DATABASE Desastres;
+
+use Desastres;
+
+CREATE TABLE desastres (
+	id_desastre VARCHAR(20) NOT NULL,
+	nombre VARCHAR (100) NOT NULL,
+	tipo VARCHAR (20) NOT NULL,
+	fecha_inicio date,
+	fecha_fin date,
+	id_region VARCHAR(20),
+CONSTRAINT pk_desastres PRIMARY KEY(id_desastre),
+CONSTRAINT fk_desastre_region FOREIGN KEY(id_region) REFERENCES regiones(id)
+)ENGINE=Innodb;
+
+CREATE TABLE regiones(
+	id_region VARCHAR(20) NOT NULL,
+	nombre VARCHAR(100) NOT NULL,
+	CONSTRAINT pk_regiones PRIMARY KEY(id_region)
+)ENGINE=Innodb;
